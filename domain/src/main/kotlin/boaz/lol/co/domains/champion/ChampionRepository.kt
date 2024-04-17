a58560f28294f972b41d07d46cde9d6821fd10c3
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-04-18 01:35:12 +0900

    feat: ChampionRepository 추상 클래스, 구현체 작성

--- domain/src/main/kotlin/boaz/lol/co/domains/champion/ChampionRepository.kt
+++ domain/src/main/kotlin/boaz/lol/co/domains/champion/ChampionRepository.kt
@@ -1,4 +1,9 @@
 package boaz.lol.co.domains.champion
 
+import java.util.Optional
+
 interface ChampionRepository {
+    fun getAll(): List<Champion>
+
+    fun getById(id: Long): Optional<Champion>
 }
