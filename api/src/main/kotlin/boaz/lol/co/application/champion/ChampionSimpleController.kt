0b07ae06eb2f771bd8fc5e99a468b5b9d66476fa
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-04-18 01:48:31 +0900

    feat: Champion 개별 조회 api

--- api/src/main/kotlin/boaz/lol/co/application/champion/ChampionSimpleController.kt
+++ api/src/main/kotlin/boaz/lol/co/application/champion/ChampionSimpleController.kt
@@ -2,8 +2,10 @@ package boaz.lol.co.application.champion
 
 import boaz.lol.co.domains.champion.Champion
 import boaz.lol.co.domains.champion.ChampionService
+import org.apache.juli.logging.Log
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,5 +17,10 @@ class ChampionSimpleController(
     @GetMapping()
     fun championFindAll(): ResponseEntity<List<Champion>> {
         return ResponseEntity.ok(championService.getChampionList());
+    }
+
+    @GetMapping("/{id}")
+    fun championFindById(@PathVariable id: Long): ResponseEntity<Champion> {
+        return ResponseEntity.ok(championService.getSingleChampion(id))
     }
 }
