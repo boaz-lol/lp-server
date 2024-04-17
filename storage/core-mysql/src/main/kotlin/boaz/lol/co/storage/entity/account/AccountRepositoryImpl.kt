2e0773acdfd6b910b33a33ba4f12dcbb043e42ff
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-04-18 01:42:25 +0900

    feat: ChampionRepository select 쿼리 부분 transactional readonly 추가

--- storage/core-mysql/src/main/kotlin/boaz/lol/co/storage/entity/account/AccountRepositoryImpl.kt
+++ storage/core-mysql/src/main/kotlin/boaz/lol/co/storage/entity/account/AccountRepositoryImpl.kt
@@ -3,12 +3,14 @@ package boaz.lol.co.storage.entity.account
 import boaz.lol.co.domains.account.Account
 import boaz.lol.co.domains.account.AccountRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 @Repository
 class AccountRepositoryImpl(
     private val accountJpaRepository: AccountJpaRepository
 ): AccountRepository {
+    @Transactional(readOnly = true)
     override fun getById(id: Long): Optional<Account> {
         return accountJpaRepository.findById(id).map(AccountEntity::to);
     }
