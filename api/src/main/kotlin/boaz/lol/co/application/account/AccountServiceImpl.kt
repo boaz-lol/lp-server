1300387e32bdc255d6dbbe7539075d11d006b273
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-05-02 02:05:24 +0900

    fix(#4): 객체 참조 시 var 키워드 private val로 변경

--- api/src/main/kotlin/boaz/lol/co/application/account/AccountServiceImpl.kt
+++ api/src/main/kotlin/boaz/lol/co/application/account/AccountServiceImpl.kt
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service
 
 @Service
 class AccountServiceImpl(
-    var accountRepository: AccountRepository
+    private val accountRepository: AccountRepository,
 ) : AccountService {
 
     override fun register(accountCreate: AccountCreate): Account {
