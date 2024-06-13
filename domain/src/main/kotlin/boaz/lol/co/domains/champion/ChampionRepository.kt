de913a51aa2fd64d94b10d45f452df506174269a
GeonWoo <69147082+Y-gw@users.noreply.github.com>
2024-06-13 12:42:52 +0900

    feat(#9): 엔티티 수정 및 도메인 계층 정의

--- domain/src/main/kotlin/boaz/lol/co/domains/champion/ChampionRepository.kt
+++ domain/src/main/kotlin/boaz/lol/co/domains/champion/ChampionRepository.kt
@@ -1,4 +1,8 @@
 package boaz.lol.co.domains.champion
 
+import boaz.lol.co.domains.account.Account
+import java.util.*
+
 interface ChampionRepository {
+    fun getById(id: Long): Optional<Champion>
 }
